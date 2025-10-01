2bb9bc5ab00724c1482baeac876e2d6f8b20c7ed
Ovidiu Voda <ovi.voda@gmail.com>
2025-10-01 17:41:46 +0300

    bumped app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "xyz.zephr.demo"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
