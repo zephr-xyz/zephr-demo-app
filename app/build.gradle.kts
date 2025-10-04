15896109c8aaf7a407d7320dab25dde19883f6fd
Ovidiu Voda <ovi.voda@gmail.com>
2025-10-04 09:33:33 +0300

    animate FOV

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "xyz.zephr.demo"
         minSdk = 31
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.0.7"
+        versionCode = 8
+        versionName = "0.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
